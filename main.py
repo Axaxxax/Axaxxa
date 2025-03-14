b8a80b0d3229a2b9ffe5dbc463f5cec795dd3c2a
Axaxxax <60062751+Axaxxax@users.noreply.github.com>
2025-03-15 00:33:21 +0500

    Update main.py

--- main.py
+++ main.py
@@ -25,16 +25,15 @@ def main():
     proxy_count = int(input("Сколько вкладок открыть? "))
 
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument('--headless=new')  # Новый headless-режим
     chrome_options.add_argument("--mute-audio")  # Отключаем звук
     chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument("--window-size=320,240")  # Минимальное разрешение
     chrome_options.add_argument("--blink-settings=imagesEnabled=false")  # Отключаем изображения
-    chrome_options.add_argument("--disable-webrtc")  # Отключаем WebRTC
-    chrome_options.add_argument("--disable-accelerated-2d-canvas")  # Отключаем анимации
-    chrome_options.add_argument("--disable-gpu")  # Отключаем GPU
     chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-software-rasterizer")
+    chrome_options.add_argument("--enable-webgl")  # Включаем WebGL
+    chrome_options.add_argument("--use-gl=default")  # Включаем стандартный OpenGL
+    chrome_options.add_argument("--disable-accelerated-2d-canvas")  # Отключаем ускоренный 2D-рендеринг
+    chrome_options.add_argument("--disable-webrtc")  # Отключаем WebRTC
 
     drivers = []
     
